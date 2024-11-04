4205e198cfe128797b441d1ff166a7ed34436154
jonathangomz <jonicgp97@gmail.com>
2024-11-04 17:05:57 -0600

    more tests for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jonathangomz.github.io',
+	base: 'karlavega',
 	integrations: [mdx(), sitemap()],
 });
